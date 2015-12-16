ff3b213c4c785a5a06ace7d2dd78a3fe7be113f3
Colton <mccurdyc@allegheny.edu>
2015-12-16 08:36:44 -0500

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to trend-compliance
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/trend_compliance/core_test.clj
@@ -0,0 +1,7 @@
+(ns trend-compliance.core-test
+  (:require [clojure.test :refer :all]
+            [trend-compliance.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
