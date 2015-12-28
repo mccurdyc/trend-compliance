b567ec5064c4feeaba62c65c2c2aa3a28474c698
Colton <mccurdyc@allegheny.edu>
2015-12-28 09:27:52 -0500

    had to revert

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.csv "0.1.3"]]
+                 [clojure-csv/clojure-csv "2.0.1"]]
   :main ^:skip-aot trend-compliance.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
