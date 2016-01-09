be6f81fcecf8ea2b06f8490d35ee3511b0803bd9
Colton <mccurdyc@allegheny.edu>
2016-01-09 07:21:55 -0500

    working with checking times to verify that time is between 20-24

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clojure-csv/clojure-csv "2.0.1"]]
+                 [clojure-csv/clojure-csv "2.0.1"]
+                 [clj-time "0.11.0"]]
   :main ^:skip-aot trend-compliance.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/trend_compliance/core.clj
+++ src/trend_compliance/core.clj
@@ -1,99 +1,101 @@
 (ns trend-compliance.core
   (:require [clojure.java.io :as io]
             [clojure.pprint :as pp]
-            [clojure.string :as str]
-            [clojure-csv.core :as csv])
+            [clojure.string :as st]
+            [clojure-csv.core :as csv]
+            [clj-time.core :as t]
+            [clj-time.format :as f])
   (:gen-class))
 
-(defn create-map
-  "parse the list of lists returned from read-csv"
-  [item]
+(def log-map (atom {}))
+(def user-map (atom {}))
+(def blank-line-regex #"^\s*$")
+(def date-regex #"(?i)\w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}")
+(def mac-regex #"(?i)Acct-Session-Id\s\=\s\"(.+)\"")
+(def ip-regex #"(?i)Framed-IP-Address\s\=\s(.+)")
+(def username-regex #"(?i)User-Name\s\=\s\"(.+)\"")
+(def timestamp-regex #"(?i)Timestamp\s\=\s\d+")
+
+(defn create-map [item]
   (let [[ip cp-name domain mac-ad off-scan-status ping prod-name platform os-server version patt-file scan-eng prev-pol cp-des rem-install] item]
     {:ip ip
      :off-scan-status off-scan-status
      :ping ping
      :platform platform}))
 
-(defn trend-installed?
-  "check to see if the device has trend"
-  [item]
+(defn trend-installed? [item]
   (if (re-matches #"(?i)not installed" (get item :off-scan-status)) true false))
 
-(defn ping-successful?
-  "check to see if the device ping was successful"
-  [item]
+(defn ping-successful? [item]
   (if (re-matches #".*(?i)ping successful" (get item :ping)) true false))
 
-(defn windows?
-  "check to see if the device is windows os"
-  [item]
+(defn windows? [item]
   (if (re-matches #"(?i)windows*" (get item :platform)) true false))
 
-(defn uncompliant-map
-  "create a list of windows machines with successful ping that fail compliance check"
-  [item]
+(defn uncompliant-map [item]
   (filter (every-pred windows? ping-successful? trend-installed?) item))
 
-(def log-map (atom {}))
-(def user-map (atom {}))
+(defn find-uncompliant-ip [ip]
+  (find @user-map ip))
 
-(defn create-log-map
-  "take a line from detail and put into map using an atom"
-  [user-map]
-  (swap! log-map assoc (keyword (get user-map :ip)) user-map))
-  ;; (swap! log-map assoc :ip (get user-map :ip))
-  ;; (swap! log-map assoc :user-name (get user-map :user-name))
-  ;; (swap! log-map assoc :mac-ad (get user-map :mac-ad))
-  ;; (swap! log-map assoc :date (get user-map :date)))
+(defn match-line [l]
+  (let [tl (st/trim l)
+        blank (re-matches blank-line-regex tl)
+        date (re-matches date-regex tl)
+        mac (re-matches mac-regex tl)
+        ip (re-matches ip-regex tl)
+        username (re-matches username-regex tl)
+        timestamp (re-matches timestamp-regex tl)]
+    (cond
+      (seq date) (list :date date)
+      (seq mac) (list :mac (re-find #"\S\S:\S\S:\S\S:\S\S:\S\S:\S\S" (second mac)))
+      (seq ip) (list :ip (second ip))
+      (seq username) (list :username (second username))
+      (not (nil? timestamp)) (list :timestamp)
+      :else (list))))
 
-(defn create-user-map
-  "parse each line of a large file"
-  [line]
-  (let [l (str/split line #" = ")]
-  (cond
-    (re-matches #"(?i)\w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}" (str (first l)))
-      (swap! user-map assoc :date (str (first l)))
-    (re-matches #"(?i)(.*)Acct-Session-Id" (str (first l)))
-      (swap! user-map assoc :Acct-Session-Id (re-find #"[^\\\"]+" (str (second l))))
-    (re-matches #"(?i)(.*)Framed-IP-Address" (str (first l)))
-      (swap! user-map assoc :Framed-IP-Address (re-find #"[^\\\"]+" (str (second l))))
-    (re-matches #"(?i)(.*)User-Name" (str (first l)))
-      (swap! user-map assoc :User-Name (re-find #"[^\\\"]+" (str (second l)))))))
+(defn create-user-map-from-file [f]
+  (with-open [rdr (io/reader f)]
+    (let [lines (line-seq rdr)]
+      (loop [l (first lines)
+             r (rest lines)
+             u {}]
+        (when l
+          (let [match (match-line l)]
+            (case (first match)
+              :timestamp (do (swap! user-map assoc (:ip u) u)
+                             (recur (first r) (rest r) {}))
+              :skip (recur (first r) (rest r) u)
+              (recur (first r) (rest r) (merge u (apply hash-map match))))))))))
 
-(defn read-large-file
-  "read in a large detail file"
-  [file-name]
-  (with-open [rdr (io/reader file-name)]
-    (let [line (line-seq rdr)
-          _ (doall (map create-user-map line))
-          user-map {:ip (get @user-map :Framed-IP-Address)
-                    :user-name (get @user-map :User-Name)
-                    :mac-ad (get @user-map :Acct-Session-Id)
-                    :date (get @user-map :date)}]
-    (when (= (count (keys user-map)) 4)
-    (create-log-map user-map)))))
-
-(defn read-csv
-  "read in a csv file"
-  [file-name]
-  (with-open [rdr (io/reader file-name)]
+(defn read-csv [f]
+  (with-open [rdr (io/reader f)]
     (doall (csv/parse-csv rdr))))
 
-(defn get-ip
-"get the ip from map"
-[item]
-(get item :ip))
+(defn get-ip [item]
+  (get item :ip))
 
-(defn -main
-  "do some fun stuff with csv files"
-  [& args]
+(defn check-time-range [item]
+  (let [date (:date (second item))
+        parse-date (f/parse (f/formatter "EEE MMM dd HH:mm:ss yyyy") date)]
+    (if (and (>= (t/hour parse-date) 20) (<= (t/hour parse-date) 24)) item "deny")))
+
+(defn -main [& args]
   (println "Files (<path/to/WIRED.csv> <path/to/WIRELSS.csv> <path/to/DETAIL>): ")
     (let [in (read-line)
-          file-names (str/split in #" ")
+          file-names (st/split in #" ")
           wired (first file-names)
           wireless (second file-names)
           detail (nth file-names 2)
-          wired-ip-list (set (map get-ip (uncompliant-map (map create-map (read-csv wired)))))
-          wireless-ip-list (set (map get-ip (uncompliant-map (map create-map (read-csv wireless)))))]
+          wired-ip-list (map get-ip (uncompliant-map (map create-map (read-csv wired))))
+          wireless-ip-list (map get-ip (uncompliant-map (map create-map (read-csv wireless))))
+          detail-user-map (create-user-map-from-file detail)
+          find-ip-list (map find-uncompliant-ip wireless-ip-list)]
+          ;; check-time-list (map check-time-range find-ip-list)]
       (println "=============== Wired List: ===============")
-      (println wired-ip-list)))
+      (doall (map pp/pprint wired-ip-list))
+      (println "=============== Wireless List: ===============")
+      (doall (map pp/pprint wireless-ip-list))
+      (println "=============== Wireless List More Detail: ===============")
+      (pp/pprint (map check-time-range find-ip-list))))
+      ;; (pp/pprint find-ip-list)))
